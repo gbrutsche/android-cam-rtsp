0b3f25090e938a115e598e3b8a066ef8f9a75a98
Gregory Brutsche <gbrutsche@novetta.com>
2017-10-20 09:58:23 -0400

    Cleanup.

--- RtspServer/src/main/java/com/optman/RtspServer/RtspServerActivitiy.java
+++ RtspServer/src/main/java/com/optman/RtspServer/RtspServerActivitiy.java
@@ -12,10 +12,7 @@ import android.view.SurfaceView;
 import android.widget.TextView;
 
 import com.optman.rtp.player.Statistics;
-import com.optman.rtp.receiver.Sample;
-import com.optman.rtp.receiver.SampleHandler;
 import com.optman.rtp.receiver.UdpServer;
-import com.optman.rtsp.TcpDataHandler;
 import com.optman.rtsp.TcpReceiver;
 
 import java.io.IOException;
@@ -25,9 +22,7 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class RtspServerActivitiy extends Activity implements Callback, StreamInfo {
-
     private SurfaceView   surfaceView;
     private SurfaceHolder surfaceHolder;
     private MediaPlayer   mediaPlayer;
@@ -44,7 +39,7 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
     private com.optman.rtp.sender.RtpAacStream   audioSenderStream;
 
 
-    private List<byte[]> spspps = new ArrayList<byte[]>();
+    private List<byte[]> spspps = new ArrayList<>();
 
     private Handler    statsHandler = new Handler();
     private Statistics st           = new Statistics();
@@ -56,7 +51,7 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
 
 
         getWindow().setFormat(PixelFormat.UNKNOWN);
-        surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        surfaceView = findViewById(R.id.surfaceview);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
 
@@ -76,37 +71,29 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
 //			public void run() {
 //		        playMovie();
 //		}})).start();
-
-
     }
 
     private void startRtp() {
 
         videoSenderStream = new com.optman.rtp.sender.RtpAvcStream(videoSessions);
 
-        videoReceiverStream = new com.optman.rtp.receiver.RtpAvcStream(new SampleHandler() {
+        videoReceiverStream = new com.optman.rtp.receiver.RtpAvcStream(sample -> {
+            //long timeUs = System.nanoTime()/1000;
+            long timeUs = sample.timestampUs;
 
-            @Override
-            public void onSample(Sample sample) {
+            byte naluType = (byte) (sample.data[4] & 0x1f);
+            if (naluType == 7 || naluType == 8) {
+                byte[] data = new byte[sample.data.length - 4];
+                System.arraycopy(sample.data, 4, data, 0, data.length);
 
-                //long timeUs = System.nanoTime()/1000;
-                long timeUs = sample.timestampUs;
-
-                byte naluType = (byte) (sample.data[4] & 0x1f);
-                if (naluType == 7 || naluType == 8) {
-                    byte[] data = new byte[sample.data.length - 4];
-                    System.arraycopy(sample.data, 4, data, 0, data.length);
-
-                    spspps.set(naluType - 7, data);
-                }
-
-                try {
-                    videoSenderStream.addNalu(sample.data, 4, sample.data.length - 4, timeUs);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                spspps.set(naluType - 7, data);
             }
 
+            try {
+                videoSenderStream.addNalu(sample.data, 4, sample.data.length - 4, timeUs);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }, st);
 
         videoReceiver = new UdpServer(/*50000*/59526, videoReceiverStream);
@@ -117,15 +104,11 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
         }
 
         audioSenderStream = new com.optman.rtp.sender.RtpAacStream(44100, audioSessions);
-        audioReceiverStream = new com.optman.rtp.receiver.RtpAacStream(44100, new SampleHandler() {
-
-            @Override
-            public void onSample(Sample sample) {
-                try {
-                    audioSenderStream.addAU(sample.data, 0, sample.data.length, sample.timestampUs);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        audioReceiverStream = new com.optman.rtp.receiver.RtpAacStream(44100, sample -> {
+            try {
+                audioSenderStream.addAU(sample.data, 0, sample.data.length, sample.timestampUs);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }, st);
 
@@ -144,49 +127,28 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
         try {
             final ServerSocket server = new ServerSocket(8001);
 
-            (new Thread(new Runnable() {
+            (new Thread(() -> {
+                while (true) {
+                    try {
+                        Socket s = server.accept();
 
-                @Override
-                public void run() {
+                        TcpReceiver client = new TcpReceiver(s);
 
-                    while (true) {
+                        client.run((channel, data, dataSize) -> {
+                            if (channel == 0)
+                                videoReceiverStream.onRtp(data, dataSize);
+                            else if (channel == 1)
+                                audioReceiverStream.onRtp(data, dataSize);
 
-                        try {
-                            Socket s = server.accept();
-
-                            TcpReceiver client = new TcpReceiver(s);
-
-                            client.run(new TcpDataHandler() {
-
-                                @Override
-                                public void onData(int channel, byte[] data,
-                                                   int dataSize) {
-
-                                    if (channel == 0)
-                                        videoReceiverStream.onRtp(data, dataSize);
-                                    else if (channel == 1)
-                                        audioReceiverStream.onRtp(data, dataSize);
-
-                                }
-
-                            });
-
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
-
             })).start();
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private void playMovie() {
@@ -205,13 +167,7 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
             mediaPlayer.setDataSource(url);
             mediaPlayer.prepare(); // might take long! (for buffering, etc)
             mediaPlayer.start();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (IllegalArgumentException | SecurityException | IllegalStateException | IOException e) {
             e.printStackTrace();
         }
     }
@@ -225,36 +181,27 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                               int height) {
-
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
     }
 
     @Override
     public List<byte[]> getSpsPps() {
-
         return spspps;
     }
 
     private void startStatsTimer() {
-        statsHandler.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                showStats();
-                startStatsTimer();
-            }
-
+        statsHandler.postDelayed(() -> {
+            showStats();
+            startStatsTimer();
         }, 1000);
     }
 
     private void showStats() {
-        TextView textView = (TextView) findViewById(R.id.infoText);
+        TextView textView = findViewById(R.id.infoText);
 
         String s = " v:\r\n" + videoSessions.dump() + "\r\n" +
                 " a:\r\n " + audioSessions.dump() +
@@ -263,7 +210,5 @@ public class RtspServerActivitiy extends Activity implements Callback, StreamInf
                 " sync " + st.syncframeCount;
 
         textView.setText(s);
-
     }
-
 }
